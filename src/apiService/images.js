4da26a14505c1bd6d0a78aa5da3030afe026ad50
Liubjchka <144176266+Liubjchka@users.noreply.github.com>
2024-04-19 23:06:59 +0300

    status 200

--- src/apiService/images.js
+++ src/apiService/images.js
@@ -1,18 +1,11 @@
 import axios from "axios";
 
-const API_KEY = "uFwH4fdMNd8GZx3KXV2zP6cZrDYT9wqBDv039lyrpUU";
-axios.defaults.baseURL = "https://api.unsplash.com/search/photos";
-axios.defaults.headers.common["Authorization"] = API_KEY;
-axios.defaults.params = {
-  per_page: 12,
-  order_by: "popular",
-  stats: true,
-  orientation: "landscape",
-};
+export const getImages = async (page, query) => {
+  const API_KEY = "uFwH4fdMNd8GZx3KXV2zP6cZrDYT9wqBDv039lyrpUU";
+  axios.defaults.baseURL = "https://api.unsplash.com/search/photos";
 
-export const getImages = async (query, page) => {
-  const { data } = await axios.get(`search?query=${query}&page=${page}`);
-  console.log(data);
-
-  return data;
+  const response = await axios.get(
+    `?client_id=${API_KEY}&page=${page}&query=${query}`
+  );
+  return response.data;
 };
